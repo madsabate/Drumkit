7b718447b7a8b7cdf9e66402fa34d2a27eda6a5c
madsabate <meynard.anthony.sabate@gmail.com>
2018-04-16 08:47:50 -0400

    Basic functions working

---
+++ assets/logic.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // Stops function from running if there is no sound clip attatched to key
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
